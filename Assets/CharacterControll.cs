82f6ebf5e2bde62a3b6a4d5ade1d5201e23050cc
Tomazio99 <tomassonim45@gmail.com>
2024-05-14 10:06:46 +0100

    Code change
    j ai changer le code mais les trucs d avant il sont en texte au cas ou

--- Assets/CharacterControll.cs
+++ Assets/CharacterControll.cs
@@ -7,15 +7,16 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Camera playerCamera;
-    public float walkSpeed = 6f;
-    public float runSpeed = 12f;
+    public float walkSpeed = 24f;
+    //public float runSpeed = 12f;
     public float jumpPower = 7f;
     public float gravity = 10f;
     public float lookSpeed = 2f;
     public float lookXLimit = 45f;
     public float defaultHeight = 2f;
     public float crouchHeight = 1f;
-    public float crouchSpeed = 3f;
+    public float crouchSpeed = 24f;
+    public float momentumFactor = 1.0f;
 
     private Vector3 moveDirection = Vector3.zero;
     private float rotationX = 0;
@@ -36,8 +37,8 @@ public class PlayerMovement : MonoBehaviour
         Vector3 right = transform.TransformDirection(Vector3.right);
 
         bool isRunning = Input.GetKey(KeyCode.LeftShift);
-        float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
-        float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
+        float curSpeedX = canMove ? walkSpeed * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? walkSpeed * Input.GetAxis("Horizontal") : 0;
         float movementDirectionY = moveDirection.y;
         moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
@@ -45,6 +46,7 @@ public class PlayerMovement : MonoBehaviour
         {
             moveDirection.y = jumpPower;
         }
+
         else
         {
             moveDirection.y = movementDirectionY;
@@ -54,19 +56,19 @@ public class PlayerMovement : MonoBehaviour
         {
             moveDirection.y -= gravity * Time.deltaTime;
         }
-
-        if (Input.GetKey(KeyCode.R) && canMove)
+        
+        if (Input.GetKey(KeyCode.CapsLock) && canMove)
         {
             characterController.height = crouchHeight;
-            walkSpeed = crouchSpeed;
-            runSpeed = crouchSpeed;
-
+            //walkSpeed = crouchSpeed;
+            //runSpeed = crouchSpeed;
+            
         }
         else
         {
             characterController.height = defaultHeight;
-            walkSpeed = 6f;
-            runSpeed = 12f;
+            walkSpeed = 24f;
+            //runSpeed = 12f;
         }
 
         characterController.Move(moveDirection * Time.deltaTime);
